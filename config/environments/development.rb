da9d680e03d91e7682c65e5090d37e28d8b76a9f
Robert Benjamin <rotberj@gmail.com>
2017-03-28 01:27:34 -0700

    Switch to HAML, setup Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Manually added as per post-install instructions
+  # In production, :host should be set to the actual host of your application
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
